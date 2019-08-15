e75cfb20d400c2f0c99ecba57e758a16ab3b1c25
Slattery Donohoe <slattery.donohoe@gmail.com>
2019-08-14 22:35:49 -0400

    Working roster scraper, tested with 2019 cardinals

--- scrape/RosterScrape.py
+++ scrape/RosterScrape.py
@@ -4,14 +4,14 @@ from bs4 import BeautifulSoup
 
 class RosterScrape:
     def __init__(self, team, year):
-        roster = []
+        self.roster = []
         url = 'https://www.baseball-reference.com/teams/' + team + '/' + str(year) + '-roster.shtml'
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         roster_data = soup.find(id='appearances').find('tbody').findAll('tr')
         for row in roster_data:
-            name = row.find('th').find('csk')
-            roster.append(name)
+            name = row.find('th')['csk']
+            self.roster.append(name)
 
 
 if __name__ == "__main__":
@@ -22,4 +22,9 @@ if __name__ == "__main__":
              'HOU', 'LAA', 'OAK', 'SEA', 'TEX',
              'ARI', 'COL', 'LAD', 'SDP', 'SFG']
     scrape = RosterScrape(teams[19], 2019)
+    success = len(scrape.roster) == 41
+    if success:
+        print("Nailed it")
+    else:
+        print("Needs work")
 
