13e03d7b7660e0d7a693c65dc250bbe8890bf609
Slattery Donohoe <slattery.donohoe@gmail.com>
2020-03-08 21:59:23 -0400

    High confidence in parsing for outs

--- retrosheet-parse/parse/games_parse.py
+++ retrosheet-parse/parse/games_parse.py
@@ -4,6 +4,24 @@ import sys
 
 filepath = "/Users/slatterydonohoe/Downloads/2018eve/"
 
+game_ids = [
+    'ANA201804020',
+    'ANA201804030',
+    'ANA201804040',
+    'ANA201804060',
+    'ANA201804070',
+    'ANA201804080',
+    'ANA201804170',
+    'ANA201804180',
+    'ANA201804190',
+    'ANA201804200',
+    'ANA201804210',
+    'ANA201804220',
+    'ANA201804270',
+    'ANA201804280',
+    'ANA201804290'
+]
+
 events = [
     'K',
     'G',
@@ -28,47 +46,29 @@ events = [
 
 ]
 
-#def parse_out(res):
-#    res_elements = res.split("/")
-#    play = ""
-#    outs = 0
-#    if len(res_elements) > 1:
-#        #if res_elements[1] in events[:10]:
-#        #    play = res_elements[1]
-#       if res_elements[1] in events[9:11]:
-#            outs += 3
-#        if res_elements[1] in events[7:9]:
-#            outs += 2
-#        if res_elements[1] in events[4:6] or res_elements[0][0].isdigit():
-#            outs += 1
-#    if res == "K":
-#        outs += 1
-#
-#    return outs
-
-
 def parse_result(res):
     res_elements = res.split('/')
     desc = res_elements[0]
     outs = 0
-    mod = res_elements[-1] if len(res_elements) > 1 else ""
+    mod = res_elements[-1] if len(res_elements) > 1 else desc
     field_event = mod.split('.')[0]
     field_play = mod.split('.')[1] if len(mod.split('.')) > 1 else ""
     if len(res_elements) > 1:
         # triple play
-        if field_event in events[9:11]:
-            outs += 3
+        if any(x in res for x in ['GTP', 'LTP', '/TP']):
+            return 3
         # double play
-        elif field_event in events[7:9]:
-            outs += 2
+        elif any(x in res for x in ['GDP', 'LDP', '/DP']):
+            return 2
         # all other outs
         elif field_event in events[4:6] or desc[0].isdigit():
             outs += 1
-    elif desc[0] == 'K':
+    if desc[0] == 'K':
         outs += 1
     # outs in the field
-
     outs += field_play.count('X')
+    # caught stealing
+    outs += desc.count('CS')
     return outs
     # regex for matching outs
 
@@ -83,40 +83,48 @@ def parse_play(play):
     return parse_result(result)
 
 
-def parse_file(file, game_id):
-    with open(file) as season_file:
-        season_reader = csv.reader(season_file, delimiter=',')
-        outs = 0
-        inning_outs = 0
-        inning = '1'
-        top = '0'
-        found_game = False
-        for row in season_reader:
-            # process game ID row
-            if row[0] == "id":
-                # Found the game we want
-                if row[1] == game_id:
-                    found_game = True
-                # Found game we don't want, but if we already found our game, this means we are done
-                elif found_game:
-                    break
-            # process play row
-            elif row[0] == "play" and found_game:
-                # log half-inning
-                if inning != row[1] or top != row[2]:
-                    print("Outs for " + ("top " if top == '0' else "bottom ") + str(inning) + ": " + str(inning_outs))
+def parse_game(reader, game_id):
+    outs = 0
+    inning_outs = 0
+    inning = '1'
+    top = '0'
+    found_game = False
+    for row in reader:
+        # process game ID row
+        if row[0] == "id":
+            # Found the game we want
+            if row[1] == game_id:
+                found_game = True
+            # Found game we don't want, but if we already found our game, this means we are done
+            elif found_game:
+                break
+        # process play row
+        elif found_game and ((row[0] == "play" and found_game) or row[0] == "data"):
+            # log half-inning
+            if inning != row[1] or top != row[2]:
+                print("Outs for " + ("top " if top == '0' else "bot ") + str(inning) + ": " + str(inning_outs))
+                if row[0] == "play":
                     inning = row[1]
                     top = row[2]
                     inning_outs = 0
+            if row[0] == "data":
+                break
+            else:
                 row_outs = parse_play(row)
                 outs += row_outs
                 inning_outs += row_outs
-        print("Outs for game " + game_id + ": " + str(outs))
+    print("Outs for game " + game_id + ": " + str(outs))
 
+
+def parse_file(file, game_id):
+    with open(file) as season_file:
+        season_reader = csv.reader(season_file, delimiter=',')
+        parse_game(season_reader, game_id)
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print("ERROR: Missing filename or game ID")
+        for g in game_ids:
+            parse_file(sys.argv[1], g)
     else:
         parse_file(sys.argv[1], sys.argv[2])
